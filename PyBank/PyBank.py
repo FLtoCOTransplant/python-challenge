0d084fa4796b92eb5f9ab64509b20017df5a341d
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-10-08 15:26:40 -0600

    working on totaling up months and revenues

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -9,22 +9,40 @@ budget_csv=os.path.join("Resources","budget_data.csv")
 t_months = 0
 t_rev = 0
 t_rev_delta = 0
+h_rev = 0
+l_rev = 0
 
 # Open and read csv
 with open(budget_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+    budget_csv = csv.reader(csv_file, delimiter=",")
 
 #Read the header and skip first row
-    csv_reader = next(csv_file)
+    budget_csv = next(csv_file)
     #print(f"Header: {csv_header}")
 
-    #start collecting data
-    for row in csv_reader:
+    #start collecting data denoting each row as a row
+    for row in budget_csv:
 
         #total months
-        t_months = t_months = 1
-        
+        t_months = t_months + 1
+
+        #total revenue
+        t_rev = t_rev + int(row[1])
+
 #Calculate the average monthly revenue
-    
-    
-    
+#ave_monthly_rev = t_rev/t_months
+
+# Provide the board with the final results 
+results = (
+    f"Financial Overview\n"
+    f"----------------------------\n"
+    f"Total # of Months : {t_months}\n"
+    f"Total Revenue for {t_months} Months: ${t_rev}\n"
+    f"Average Monthly Revenue : $\n"
+
+
+
+)    
+
+#Show output in terminal
+print(results)
