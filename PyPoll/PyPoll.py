06a40e2b19ec3296013d5791fbb7d90b44797309
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-10-09 10:49:22 -0600

    Able to print results, working on max

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -10,50 +10,53 @@ t_votes = 0
 candidate = {}
 
 #File output
-analysis = os.path.join("Resources", "Voting Results for Small Town, USA.txt")
+analysis = os.path.join("Voting Results for Small_Town, USA.txt")
 
 # Open and read csv
 with open(vote_csv) as csv_file:
-    vote_csv = csv.reader(csv_file, delimiter=",")
+    vote_reader = csv.reader(csv_file, delimiter=",")
 
 #Read the header and skip first row
-    vote_csv = next(csv_file)
+    header = next(vote_reader)
     #print(f"Header: {budget_csv}")   
     
     #start collecting data denoting each row
-    for row in vote_csv:  
+    for row in vote_reader:  
 
         #determine total votes
         t_votes+= 1
 
         #Total up the votes for each candidate as we collect candicate names
-        if row[0] in candidate:
-            candidate[row[0]]+= 1
+        if row[2] in candidate:
+            candidate[row[2]]+= 1
         #if that candidate is not in the list add them
         else:
-            candidate[row[0]] = 1
-
-#Calc the percent of each candidates votes
-for key,value in candidate.items():
-    candidate[key] = str(round((value/t_votes)*100,3))+ "%" + " ("+str(value)+ ")"
-
-#Who is the winner
-winner = max(candidate.keys())
-
-# Provide the public with the results of the election 
-results = (
-    f"      Election Results\n"
-    f"----------------------------\n"
-    f"Total # of votes cast : {t_votes}\n"
-    f"----------------------------\n"
-    f"Result for Each Candidate\n"
-    f"OUTPUT RESULTS HERE : $\n"
-    f"Winner of the Election is: {winner}\n"
-)    
-
-#Show output in terminal
-print(results)
+            candidate[row[2]] = 1
 
 #Output results to file
 with open(analysis, "w") as txt_file:
+
+    #Who is the winner
+    winner = max(candidate.keys())
+
+    # Provide the public with the results of the election 
+    results = (
+        f"      Election Results\n"
+        f"----------------------------\n"
+        f"Total # of votes cast : {t_votes}\n"
+        f"----------------------------\n"
+        f"Result for Each Candidate\n"
+        
+    )           
+
+    #Show output in terminal
+    print(results)
     txt_file.write(results)
+
+        #Calc the percent of each candidates votes
+    for key,value in candidate.items():
+        perc_vote = str(round((value/t_votes)*100,3))+ "%" + " ("+str(value)+ ")"
+        print (f"Results for {key} - {perc_vote}")
+    print (f"Winner of the Election is: {winner}\n")
+        
+            
